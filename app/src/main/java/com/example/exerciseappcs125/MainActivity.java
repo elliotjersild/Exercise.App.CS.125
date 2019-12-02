5f2e01c4dc245bb9fb9fc21f952570000bc86369
Elliot Jersild <jersild2@illinois.edu>
2019-12-02 17:42:54 -0600

    Buttons

--- app/src/main/java/com/example/exerciseappcs125/MainActivity.java
+++ app/src/main/java/com/example/exerciseappcs125/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.exerciseappcs125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button newRun = findViewById(R.id.newRun);
+        Button viewRun = findViewById(R.id.viewRuns);
+        newRun.setOnClickListener(unused -> newRunClicked());
+        viewRun.setOnClickListener(unused -> viewRunClicked());
+    }
+    private void newRunClicked() {
+
+    }
+    private void viewRunClicked() {
+
     }
 }
