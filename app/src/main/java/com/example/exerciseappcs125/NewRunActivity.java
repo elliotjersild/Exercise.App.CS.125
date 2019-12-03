d52d88a643cb4a5542a217ea86b65db08d8ee76e
Elliot Jersild <jersild2@illinois.edu>
2019-12-02 18:10:48 -0600

    Buttons 2

---
+++ app/src/main/java/com/example/exerciseappcs125/NewRunActivity.java
@@ -0,0 +1,14 @@
+package com.example.exerciseappcs125;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewRunActivity extends AppCompatActivity {
+
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newrun);
+
+    }
+}
