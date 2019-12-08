f7ae1af62473d317be67c32cccb37d87e2557120
Elliot Jersild <jersild2@illinois.edu>
2019-12-08 15:10:19 -0600

    finished map

--- app/src/main/java/com/example/exerciseappcs125/CalenderActivity.java
+++ app/src/main/java/com/example/exerciseappcs125/CalenderActivity.java
@@ -3,6 +3,7 @@ package com.example.exerciseappcs125;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
@@ -10,7 +11,12 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CalenderActivity extends AppCompatActivity {
+    //private NewRunActivity runActivity = new NewRunActivity();
+    private Map<String, String> map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,11 @@ public class CalenderActivity extends AppCompatActivity {
         TextView run = (TextView) findViewById(R.id.runTime);
         date.setVisibility(View.GONE);
         run.setVisibility(View.GONE);
+        Intent intent = getIntent();
+        String date1 = intent.getStringExtra("Date");
+        String run1 = intent.getStringExtra("Run");
+
+
         calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             /**
@@ -28,9 +39,33 @@ public class CalenderActivity extends AppCompatActivity {
              */
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 String dateToDisplay = (month + 1) + "/" + day + "/" + year;
-                date.setText(dateToDisplay);
-                date.setVisibility(View.VISIBLE);
+                //updateMap();
+                System.out.println("date to display: " + dateToDisplay);
+                System.out.println("Map in calender " + NewRunActivity.storeRuns.keySet() + " " +
+                        NewRunActivity.storeRuns.entrySet());
+                for (Map.Entry<String, String> entry : NewRunActivity.storeRuns.entrySet()) {
+                    System.out.println("Before if statement");
+
+                    if (entry.getKey().equals(dateToDisplay)) {
+                        run.setText(entry.getValue());
+                        run.setVisibility(View.VISIBLE);
+                        date.setText(dateToDisplay);
+                        date.setVisibility(View.VISIBLE);
+                        System.out.println("String date" + dateToDisplay + "Map date:" +
+                                entry.getKey() + "Time ran " + entry.getValue());
+                    } else {
+                        date.setText("Did not run on this date.");
+                        run.setVisibility(View.GONE);
+                    }
+                }
             }
         });
     }
+    /**
+    public void updateMap() {
+        Intent intent = getIntent();
+        String date = intent.getStringExtra("Date");
+        String run = intent.getStringExtra("Run");
+        storeRuns.put(date, run);
+    } */
 }
--- app/src/main/java/com/example/exerciseappcs125/NewRunActivity.java
+++ app/src/main/java/com/example/exerciseappcs125/NewRunActivity.java
@@ -1,8 +1,10 @@
 package com.example.exerciseappcs125;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +19,9 @@ public class NewRunActivity extends AppCompatActivity {
 
     private String timedRun;
 
-    Map<String, String> storeRuns = new HashMap<>();
+    public static Map<String, String> storeRuns = new HashMap<>();
+
+
 
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,29 +32,19 @@ public class NewRunActivity extends AppCompatActivity {
             EditText date = (EditText) findViewById(R.id.dateEntered);
             EditText timeRan = (EditText) findViewById(R.id.timeOfRun);
             ConstraintLayout calender = findViewById(R.id.calenderGroup);
-            String storeDate = date.toString();
-            String storeRun = timeRan.toString();
-            System.out.println("You entered " + storeDate + " " + storeRun);
-            View chunkRun = getLayoutInflater().inflate(R.layout.chunk_run_info, calender, false);
+            String storeDate = date.getText().toString();
+            String storeRun = timeRan.getText().toString();
+            storeRuns.put(storeDate, storeRun);
+            System.out.println("Map in new run " + storeRuns.keySet() + " " + storeRuns.entrySet());
+            //System.out.println("You entered " + storeDate + " " + storeRun);
+            Intent intent = new Intent(this, CalenderActivity.class);
+            intent.putExtra("Date", storeDate);
+            intent.putExtra("Run", storeRun);
+
+            //System.out.println("Map has " + storeRuns.keySet() + " " + storeRuns.entrySet());
+            //View chunkRun = getLayoutInflater().inflate(R.layout.chunk_run_info, calender, false);
 
         });
-
-       // LinearLayout invGroup = findViewById(R.id.invitationsGroup);
-        //                    invGroup.setVisibility(View.VISIBLE);
-        //                    System.out.println("set Invitations as visible");
-        //                    View chunkInv = getLayoutInflater().inflate(R.layout.chunk_invitations, invitations, false);
-        //                    TextView address = chunkInv.findViewById(R.id.invEmail);
-        //                    address.setText(email);
-        //                    TextView mmode = chunkInv.findViewById(R.id.invMode);
-        //                    mmode.setText(gMode + " mode");
-        //                    TextView teamname = chunkInv.findViewById(R.id.TeamName);
-        //                    teamname.setText(teamNames[teamm]);
-        //                    Button accept = chunkInv.findViewById(R.id.accept);
-        //                    Button decline = chunkInv.findViewById(R.id.decline);
-        //                    accept.setOnClickListener(unused -> accept(gameID));
-        //                    decline.setOnClickListener(unused -> decline(gameID));
-        //
-        //                    invitations.addView(chunkInv);
     }
 
 }
